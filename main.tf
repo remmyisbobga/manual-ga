d89f4887c0e1fd4319400eb939a7b9b460c42fb2
remmyisbobga <remmyisbobga@gmail.com>
2024-11-18 23:50:57 -0500

    husband

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-# main.tf
-provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file(var.google_credentials_file)
+# # main.tf
+# provider "google" {
+#   project     = var.project_id
+#   region      = var.region
+#   credentials = file(var.google_credentials_file)
 
-}
+# }
 
-resource "google_storage_bucket" "example_bucket" {
-  name          = "${var.bucket_name}-${random_id.unique_id.hex}"
-  location      = var.region
-  storage_class = "STANDARD"
-}
+# resource "google_storage_bucket" "example_bucket" {
+#   name          = "${var.bucket_name}-${random_id.unique_id.hex}"
+#   location      = var.region
+#   storage_class = "STANDARD"
+# }
 
-resource "random_id" "unique_id" {
-  byte_length = 4
-}
+# resource "random_id" "unique_id" {
+#   byte_length = 4
+# }
 
 
