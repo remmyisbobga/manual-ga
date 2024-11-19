f14a47f8efabff57afb5326e2b657c3407aad036
remmyisbobga <remmyisbobga@gmail.com>
2024-11-18 22:32:38 -0500

    added

--- variable.tf
+++ variable.tf
@@ -1,24 +1,28 @@
 # variables.tf
+
+# The GCP Project ID
 variable "project_id" {
   description = "The GCP Project ID"
   type        = string
+  default     = "myzesoftlyproject27" # Default value for project ID
 }
 
+# The GCP region
 variable "region" {
   description = "The GCP region"
   type        = string
   default     = "us-central1" # Optional default value
 }
 
+# The name of the GCS bucket
 variable "bucket_name" {
   description = "The name of the GCS bucket"
   type        = string
 }
 
+# Path to the Google Cloud credentials file
 variable "google_credentials_file" {
-  default = "/home/runner/gcloud-key.json"
-}
-
-variable "project_id" {
-  default = "myzesoftlyproject27"
+  description = "The path to the Google Cloud credentials file"
+  type        = string
+  default     = "/home/runner/gcloud-key.json"
 }
