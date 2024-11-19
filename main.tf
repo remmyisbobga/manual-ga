37affab4263c261b821c7575ea2902ac6e1bad32
remmyisbobga <remmyisbobga@gmail.com>
2024-11-18 23:37:37 -0500

    Nji cong

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@
 provider "google" {
   project = var.project_id
   region  = var.region
+  credentials = file(var.google_credentials_file)
+
 }
 
 resource "google_storage_bucket" "example_bucket" {
